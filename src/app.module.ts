fbc464d4b5aca0efa0af690d6444d7aa56a50221
dtduc-ptit <dtduc.ptit@gmail.com>
2025-06-19 16:24:59 +0700

    chore: build Redis cho hệ thống

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './modules/config/typeorm.config';
 import { AuthModule } from './modules/auth/auth.module';
+import { createKeyv } from '@keyv/redis';
+import { Keyv } from 'keyv';
+import { CacheableMemory } from 'cacheable';
+import { CacheModule } from '@nestjs/cache-manager';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          stores: [
+            createKeyv('redis://redis:6379'),
+          ],
+        };
+      },
+    }),
     AuthModule,
   ],
   controllers: [],
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException, UnauthorizedException } from "@nestjs/common";
+import { Injectable, ConflictException, UnauthorizedException, Inject } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
@@ -6,6 +6,8 @@ import { RegisterDto } from "./dto/register.dto";
 import { LoginDto } from "./dto/login.dto";
 import * as bcrypt from 'bcrypt';
 import { JwtService } from "@nestjs/jwt";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class AuthService {
@@ -13,6 +15,7 @@ export class AuthService {
         @InjectRepository(User)
         private readonly userRepository:  Repository<User>,
         private readonly jwtService: JwtService, 
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
     ) {}
 
     async register(registerDto: RegisterDto) {
@@ -36,10 +39,15 @@ export class AuthService {
 
     async login(loginDto: LoginDto) {
         const user = await this.userRepository.findOne({where: { email: loginDto.email }});
-
+        
         if (!user || (await bcrypt.compare(loginDto.password, user.password)) === false) {
             throw new UnauthorizedException('Invalid email or password');
         }
+
+        // await this.cacheManager.set('test', 'test value', 3600000);
+
+        // const value = await this.cacheManager.get('test');
+        // console.log('Redis get test:', value);
 
         const payload = { email: user.email, sub: user.id };
         const token = this.jwtService.sign(payload);
