0fe7b0f8f206e589b4f123f6388485fbe9fff33c
dtduc-ptit <dtduc.ptit@gmail.com>
2025-06-20 16:59:10 +0700

    refactor: Cải thiện entity cho score

--- src/modules/scores/entitis/scores.entity.ts
+++ src/modules/scores/entitis/scores.entity.ts
@@ -1,18 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from "typeorm";
 import { User } from '../../auth/entities/user.entity';
 import { Game } from '../../game/entitis/game.entity'
 
 @Entity()
 export class Score {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column()
-	score: number;
+  @Column()
+  score: number;
 
-	@ManyToOne(() => User, (user) => user.scores)
-	user: User;
+  @ManyToOne(() => User, (user) => user.scores, { onDelete: 'CASCADE' })
+  user: User;
 
-	@ManyToOne(() => Game, (game) => game.scores)
-	game: Game;
+  @ManyToOne(() => Game, (game) => game.scores, { onDelete: 'CASCADE' })
+  game: Game;
+
+  @CreateDateColumn()
+  createdAt: Date;
 }
