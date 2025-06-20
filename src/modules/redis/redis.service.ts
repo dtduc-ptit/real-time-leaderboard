f7151f7643151e85f38a2234c8d3ab276da5c74f
dtduc-ptit <dtduc.ptit@gmail.com>
2025-06-20 17:03:26 +0700

    feat: Cấu hình và viết service cho Redis

---
+++ src/modules/redis/redis.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import Redis from 'ioredis';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+@Injectable()
+export class RedisService {
+
+    private readonly client: Redis;
+    constructor() {
+    this.client = new Redis({
+        host: process.env.REDIS_HOST, 
+        port: 6379,
+    });
+    }
+
+    async addScore(key: string, score: number, userId: number) {
+        return this.client.zadd(key, score, userId.toString());
+    }
+
+    async getTop(key: string, count: number) {
+        return await this.client.zrevrange(key, 0, count - 1, 'WITHSCORES');
+    }
+
+    async getRank(key: string, userId: number) {
+        const rank = await this.client.zrevrank(key, userId.toString());
+        const score = await this.client.zscore(key, userId.toString());
+
+        return {
+        rank: rank !== null ? rank + 1 : null,
+        score: score !== null ? Number(score) : null,
+        };
+    }
+
+    async getAllKeys(key: string) {
+        return this.client.keys(key);
+    }
+}
