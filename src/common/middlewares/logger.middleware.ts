d99824741a9896bd03b1e66cb2a8231e11bf49f2
dtduc-ptit <dtduc.ptit@gmail.com>
2025-06-24 15:27:34 +0700

    fix: fix bug log body req lỗi underfind

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -7,8 +7,8 @@ export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         // log request
         console.log(`Request: ${req.method} ${req.originalUrl}`);
-        if(Object.keys(req.body).length) {
-            console.log('Request body: ', req.body);
+        if (req.body && Object.keys(req.body).length > 0) {
+            console.log('Request body:', req.body);
         }
 
         // log Response
